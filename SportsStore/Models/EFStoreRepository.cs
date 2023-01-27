ceb0d42873b5f8cd2a964da5733043cb1c16d518
Dmitriy Listopad <dimalist2005@gmail.com>
2023-01-27 03:10:03 +0300

    Create implementation of repository interface

---
+++ SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsStore.Models
+{
+    public class EFStoreRepository: IStoreRepository
+    {
+        private StoreDbContext context;
+        public EFStoreRepository(StoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
