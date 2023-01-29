187764f30e57a17a3f5a3478730794a6121ca1b6
Dmitriy Listopad <dimalist2005@gmail.com>
2023-01-29 20:12:35 +0300

    Add Act and Assert

--- SportsStore.Tests/HomeControllerTest.cs
+++ SportsStore.Tests/HomeControllerTest.cs
@@ -4,6 +4,7 @@ using SportsStore;
 using Moq;
 using SportsStore.Models;
 using SportsStore.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 public class HomeControllerTest
 {
@@ -19,6 +20,15 @@ public class HomeControllerTest
 
         HomeController controller = new HomeController(mock.Object);
 
-        
+        //Act
+        IEnumerable<Product>? result = (controller.Index() as ViewResult)?.ViewData.Model 
+            as IEnumerable<Product>;
+
+        //Assert
+        Product[] prodArray = result?.ToArray() ?? Array.Empty<Product>();
+
+        Assert.True(prodArray.Length== 2);
+        Assert.Equal("P1", prodArray[0].Name);
+        Assert.Equal("P2", prodArray[1].Name);
     }
 }
