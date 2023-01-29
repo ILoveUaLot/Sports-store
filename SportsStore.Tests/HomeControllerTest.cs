ee5f01f984044e4cd4ed882a820c381012e18422
Dmitriy Listopad <dimalist2005@gmail.com>
2023-01-29 19:54:03 +0300

    Setup mock with queryable of products

--- SportsStore.Tests/HomeControllerTest.cs
+++ SportsStore.Tests/HomeControllerTest.cs
@@ -1,11 +1,19 @@
 namespace SportsStore.Tests;
 using Xunit;
 using SportsStore;
+using Moq;
+using SportsStore.Models;
+
 public class HomeControllerTest
 {
     [Fact]
-    public void Test1()
+    public void Can_Use_Repository()
     {
-
+        Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+        mock.Setup(m => m.Products).Returns((new Product[]
+        {
+            new Product {ProductID = 1, Name = "P1"},
+            new Product {ProductID = 2, Name = "P2"}
+        }).AsQueryable<Product>());
     }
 }
