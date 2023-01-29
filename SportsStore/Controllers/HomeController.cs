00fc3617f18988385c167e6734080168004beb90
Dmitriy Listopad <dimalist2005@gmail.com>
2023-01-29 19:36:15 +0300

    Output list of products

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class HomeController: Controller
     {
-        public IActionResult Index() => View();
+        private IStoreRepository repository;
+        public HomeController(IStoreRepository repo)
+        {
+            repository= repo;
+        }
+        public IActionResult Index() => View(repository.Products);
     }
 }
