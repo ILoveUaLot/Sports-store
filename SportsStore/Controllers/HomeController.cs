b4c0ce14d393582f1e55313a9b4214df406a38a5
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-02 01:39:54 +0300

    Update homecontroller for ProductsListViewModel

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -13,10 +14,19 @@ namespace SportsStore.Controllers
         }
         public IActionResult Index(int productPage=1)
         {
-            return View(repository.Products
-                .OrderBy(p=>p.ProductID)
-                .Skip((productPage-1) * PageSize)
-                .Take(PageSize));
+            return View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                            .OrderBy(p => p.ProductID)
+                            .Skip((productPage-1)*PageSize)
+                            .Take(PageSize),
+                        PagingInfo = new PagingInfo
+                        {
+                            CurrentPage= productPage,
+                            ItemsPerPage=PageSize,
+                            TotalItems = repository.Products.Count()
+                        }
+            });
         }
     }
 }
