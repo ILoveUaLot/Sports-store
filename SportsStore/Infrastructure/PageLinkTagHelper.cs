96a9038761675b6715de7c6b288341ce87be9fda
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-01 23:52:23 +0300

    Create PageLinkTagHelper class

---
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SportsStore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper: TagHelper
+    {
+        
+
+    }
+}
