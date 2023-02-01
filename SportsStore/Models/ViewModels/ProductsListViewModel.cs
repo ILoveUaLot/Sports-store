bc5da7272aa5eb89715fcbd783c47badea8c5caa
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-02 01:36:39 +0300

    Add Products sequence and PagingInfo fields

--- SportsStore/Models/ViewModels/ProductsListViewModel.cs
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -2,5 +2,8 @@
 {
     public class ProductsListViewModel
     {
+        public IEnumerable<Product> Products { get; set;}
+            = Enumerable.Empty<Product>();
+        public PagingInfo PagingInfo { get; set; } = new();
     }
 }
