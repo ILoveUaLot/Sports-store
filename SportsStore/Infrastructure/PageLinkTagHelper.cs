72730f25f643d88fd445002cfab773ec2d8d206a
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-08 03:09:21 +0300

    Added prefixed values into tag helper

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -23,6 +23,9 @@ namespace SportsStore.Infrastructure
         public PagingInfo? PageModel { get; set; }
         public string? PageAction { get; set; }
 
+        [HtmlAttributeName(DictionaryAttributePrefix ="page-url-")]
+        public Dictionary<string,object> PageUrlValues { get; set; } = new Dictionary<string,object>();
+
         public bool PageClassEnabled { get; set; } = false;
         public string PageClass { get; set; } = String.Empty;
         public string PageClassNormal { get; set; } = String.Empty;
@@ -36,6 +39,8 @@ namespace SportsStore.Infrastructure
                 for(int i = 1; i <= PageModel.TotalPages; i++)
                 {
                     TagBuilder tag = new TagBuilder("a");
+                    PageUrlValues["productPage"] = i;
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
                     tag.Attributes["href"] = urlHelper.Action(PageAction, 
                         new { productPage = i });
                     if (PageClassEnabled)
