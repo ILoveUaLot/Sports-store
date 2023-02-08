7af4ec195c91a1076125827b07ee5f86e9be6518
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-09 02:15:34 +0300

    Added Arrange

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Moq;
+using SportsStore.Components;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +17,17 @@ namespace SportsStore.Tests
         [Fact]
         public void Can_select_categories()
         {
+            //Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(x => x.Products).Returns((new Product[]
+            {
+                new Product{Name="P1", ProductID=1,Category="Apples"},
+                new Product{Name="P2", ProductID=2,Category="Apples"},
+                new Product{Name="P3", ProductID=3,Category="Plums"},
+                new Product{Name="P4", ProductID=4,Category="Oranges"}
+            }).AsQueryable<Product>());
 
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
         }
     }
 }
