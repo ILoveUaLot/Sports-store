533284a5618e245f9eb7e935188985279227a7f4
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-11 01:45:59 +0300

    Fixed wrong output of page count

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -25,7 +25,10 @@ namespace SportsStore.Controllers
                             {
                                 CurrentPage= productPage,
                                 ItemsPerPage=PageSize,
-                                TotalItems = repository.Products.Count()
+                                TotalItems = category==null ? repository.Products.Count()
+                                                            : repository.Products
+                                                                .Where(p=> p.Category==category)
+                                                                .Count()
                             },
                             CurrentCategory=category
             });
