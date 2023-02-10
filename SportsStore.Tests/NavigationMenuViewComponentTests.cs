bc62254a498c16ab6c3a19bce0918a86af96a687
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-10 03:31:52 +0300

    Wrote arrange

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Moq;
 using SportsStore.Components;
@@ -47,7 +48,23 @@ namespace SportsStore.Tests
         [Fact]
         public void Indicates_Selected_Category()
         {
+            //Arrange
+            string categoryToSelect = "Apple";
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+            new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+            new Product {ProductID = 4, Name = "P2", Category = "Oranges"},
+            }).AsQueryable<Product>());
 
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+            target.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new ViewContext
+                {
+                    RouteData = new Microsoft.AspNetCore.Routing.RouteData()
+                }
+            };
+            target.RouteData.Values["category"] = categoryToSelect;
         }
     }
 }
