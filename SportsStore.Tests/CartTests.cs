083a93abd91cfab8e599233e06e5b97310981c4b
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-14 02:58:41 +0300

    Add Arrange and Act

---
+++ SportsStore.Tests/CartTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using System.Linq;
+using Moq;
+using SportsStore.Models;
+namespace SportsStore.Tests
+{
+    public class CartTests
+    {
+        [Fact]
+        public void Can_Add_New_Lines()
+        {
+            //Arrange - create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1"};
+            Product p2 = new Product { ProductID =2, Name="P2"};
+
+            //Arrange - create a new cart
+            Cart target = new Cart();
+
+            //Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            CartLine[] results = target.Lines.ToArray();
+
+            //
+        }
+    }
+}
