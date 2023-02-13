06909ab80f30c27fbf50c1c104e5d594373a7fd3
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-14 01:55:34 +0300

    Deleted useless string

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -41,8 +41,6 @@ namespace SportsStore.Infrastructure
                     TagBuilder tag = new TagBuilder("a");
                     PageUrlValues["productPage"] = i;
                     tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
-                    tag.Attributes["href"] = urlHelper.Action(PageAction, 
-                        new { productPage = i });
                     if (PageClassEnabled)
                     {
                         tag.AddCssClass(PageClass);
