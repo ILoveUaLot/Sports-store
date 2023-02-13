4b98ae18368ef4a09e1ba8085182522c36ad3536
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-14 02:38:48 +0300

    Added CartLine

--- SportsStore/Models/Cart.cs
+++ SportsStore/Models/Cart.cs
@@ -2,5 +2,13 @@
 {
     public class Cart
     {
+        
+    }
+
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; } = new();
+        public int Quantity { get; set; }
     }
 }
