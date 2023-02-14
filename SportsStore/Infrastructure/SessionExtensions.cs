736ffe0623b290f002e7de83bb8011e12a0cfa63
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-14 03:25:54 +0300

    Added SetJson method

---
+++ SportsStore/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace SportsStore.Infrastructure
+{
+    public static class SessionExtensions
+    {
+        public static void SetJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+    }
+}
