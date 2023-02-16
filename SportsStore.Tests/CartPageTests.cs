9e787e26e79743bce94502a558ff19bdeb3abde4
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-16 06:32:25 +0300

    Wrote Arrange for Can_Load_Cart

--- SportsStore.Tests/CartPageTests.cs
+++ SportsStore.Tests/CartPageTests.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using SportsStore.Models;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
+using Xunit;
 
 namespace SportsStore.Tests
 {
     public class CartPageTests
     {
+        [Fact]
+        public void Can_Load_Cart()
+        {
+            //Arrange
+            Product p1 = new Product { Name = "P1", ProductID = 1 };
+            Product p2 = new Product { Name = "P2", ProductID = 2 };
+
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                p1,
+                p2
+            }.AsQueryable<Product>());
+
+            //create cart
+            Cart TestCart = new Cart();
+            TestCart.AddItem(p1, 2);
+            TestCart.AddItem(p2, 1);
+            //Create mock page context and session
+            Mock<ISession> mockSession = new Mock<ISession>();
+            byte[] data =
+                Encoding.UTF8.GetBytes(JsonSerializer.Serialize(TestCart));
+            mockSession.Setup(c => c.TryGetValue(It.IsAny<string>(), out data));
+
+            Mock<HttpContext> mockContext = new Mock<HttpContext>();
+            mockContext.Setup(c=> c.Session).Returns(mockSession.Object);
+
+            //Action
+        }
     }
 }
